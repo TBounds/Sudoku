2e865a78b7ba5348525799bc999abdfc9f00ae07
Tyler James Bounds <t.bounds@ecs325-9.labs.encs>
2017-02-07 10:00:57 -0800

    init commit

---
+++ SudokuLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SudokuLayout
+//
+//  Created by Tyler James Bounds on 2/7/17.
+//  Copyright © 2017 Tyler James Bounds. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
